ac4f925ba027c6533ce99e102cca3c5970e130ec
Volcanoacid <alexismartinezmed31@gmail.com>
2024-08-30 02:28:01 -0600

    yabadabadoo

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
 
+//Type interface to describe a user
+export interface User {
+  ID?: number;
+  name: string;
+  username: string;
+  password: string;
+  role: string;
+}
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private readonly users = <User[]>[
+    {
+      ID: 1,
+      name: 'john carter',
+      username: 'jdawg',
+      password: 'fire',
+      role: 'Aspirante',
+    },
+    {
+      ID: 2,
+      name: 'sabrina rina',
+      username: 'Srina',
+      password: 'rinabrina',
+    },
+  ];
+
+  async findOne(username: string): Promise<User | undefined> {
+    return this.users.find((user) => user.username === username);
+  }
+}
